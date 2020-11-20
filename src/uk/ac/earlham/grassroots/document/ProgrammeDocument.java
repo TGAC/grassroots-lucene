41b0211e5f4c565ac6c09d5228d9a02f14df3f85
Simon Tyrrell <tyrrell.simon@gmail.com>
2020-11-20 14:22:36 +0000

    refactoring

---
+++ src/uk/ac/earlham/grassroots/document/ProgrammeDocument.java
@@ -0,0 +1,56 @@
+package uk.ac.earlham.grassroots.document;
+
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+
+import uk.ac.earlham.grassroots.document.util.DocumentWrapper;
+
+public class ProgrammeDocument extends MongoDocument {
+	final static private String PD_PREFIX = "programme-";
+	final static private String PD_ABBREVIATION = PD_PREFIX + "abbreviation";
+	final static private String PD_CROP = PD_PREFIX + "crop";
+	final static private String PD_PI = PD_PREFIX + "pi";
+	final static private String PD_URL = PD_PREFIX + "url";
+
+	
+	public ProgrammeDocument (JSONObject json_doc, DocumentWrapper wrapper) throws IllegalArgumentException {
+		super (json_doc, wrapper);
+	}
+
+	
+	@Override
+	public String getUserFriendlyTypename() {
+		return "Program";
+	}
+
+	protected boolean addFields (JSONObject json_doc) {
+		boolean success_flag = false;
+		
+		if (super.addFields (json_doc)) {
+			
+			addString (json_doc, "so:alternateName", PD_ABBREVIATION);
+			addString (json_doc, "crop", PD_CROP);
+			addText (json_doc, "principal_investigator", PD_PI);
+			addString (json_doc, "so:url", PD_URL);
+					
+			success_flag = true;
+		}
+	
+		return success_flag;
+	}
+	
+	
+	static public void addQueryTerms (List <String> fields, Map <String, Float> boosts) {
+		fields.add (PD_ABBREVIATION);
+		fields.add (PD_CROP);
+		fields.add (PD_PI);
+		fields.add (PD_URL);
+	
+		if (boosts != null) {
+			boosts.put (PD_ABBREVIATION, GD_DESCRIPTION_BOOST);
+		}
+	}
+
+}
